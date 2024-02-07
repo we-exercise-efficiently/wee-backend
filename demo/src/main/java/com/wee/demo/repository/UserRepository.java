9efbc1ec5dc2dcb4b448fd32b0cb10ffc58cb0e5
daeun0220 <daeunkim1284@gmail.com>
2024-02-07 14:37:39 +0900

    #11- userRepository conflict

--- demo/src/main/java/com/wee/demo/repository/UserRepository.java
+++ demo/src/main/java/com/wee/demo/repository/UserRepository.java
@@ -1,17 +1,8 @@
 package com.wee.demo.repository;
 
-<<<<<<< HEAD
-import com.wee.demo.domain.Todo;
 import com.wee.demo.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
-=======
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.wee.demo.domain.User;
 import org.springframework.transaction.annotation.Transactional;
-
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
@@ -21,4 +12,3 @@ public interface UserRepository extends JpaRepository<User, Long> {
     void deleteByUserId(Long userId);
 }
 
->>>>>>> 12eb692bfdcbdd5802e67bbb6eb01b91b6f93b77
