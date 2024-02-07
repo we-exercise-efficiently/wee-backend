6251a2b080ded5d03716ec4d0247e601143d7f02
0woogie <aod8948@naver.com>
2024-02-07 15:27:28 +0900

    [FIX] 충돌해결

--- demo/src/main/java/com/wee/demo/controller/CommunityController.java
+++ demo/src/main/java/com/wee/demo/controller/CommunityController.java
@@ -1,20 +1,25 @@
 package com.wee.demo.controller;
 
+import com.wee.demo.domain.community.QuestionCommunity;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 
+@RestController
+@Slf4j
 @RequestMapping("/comm")
 public class CommunityController {
 
     //운동질문방 글 작성
     @PostMapping("/question")
-    public ResponseEntity write(@RequestBody HashMap<String, Object> requestJsonHashMap) throws Exception{
-        System.out.println("write complete");
+    public ResponseEntity write(@RequestBody QuestionCommunity qc) throws Exception{
+        log.info("get parameter" + qc.getId());
         return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
 }
