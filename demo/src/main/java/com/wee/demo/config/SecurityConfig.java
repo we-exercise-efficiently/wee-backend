35a5a48d556d8c9bce0cc52f52ee8adfa969a6ff
Kang Hye Ri <121994108+merrykang@users.noreply.github.com>
2024-02-13 22:09:12 +0900

    Chore: 프론트와 통신 테스트

--- demo/src/main/java/com/wee/demo/config/SecurityConfig.java
+++ demo/src/main/java/com/wee/demo/config/SecurityConfig.java
@@ -60,6 +60,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
 
 @Configuration
 @EnableWebSecurity
