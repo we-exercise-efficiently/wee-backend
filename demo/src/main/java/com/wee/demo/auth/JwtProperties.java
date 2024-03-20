0eb1993db951d0e5831c885792211dd8ebd67b9a
Kang Hye Ri <121994108+merrykang@users.noreply.github.com>
2024-03-20 13:46:12 +0900

    fix: 프론트와 리프레쉬 통신 테스트

--- demo/src/main/java/com/wee/demo/auth/JwtProperties.java
+++ demo/src/main/java/com/wee/demo/auth/JwtProperties.java
@@ -2,7 +2,7 @@ package com.wee.demo.auth;
 
 
 public class JwtProperties {
-    public final static int ACCESS_TOKEN_EXPIRATION_TIME = 10; //for test
+    public final static int ACCESS_TOKEN_EXPIRATION_TIME = 1000; //for test
     public final static int REFRESH_TOKEN_EXPIRATION_TIME = 60 * 60 * 60 * 24 * 30 * 1000; //1개월
     public final static String TOKEN_PREFIX = "Bearer ";
     public final static String ACCESS_HEADER_STRING = "Authorization";
